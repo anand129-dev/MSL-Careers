fa127c2b41911f2e5f89a4b886bc15dcdf6e26da
Anand <anand@git.system>
2025-12-09 20:39:23 +0530

    All API working, just the cutome ID is not being generated propely, also few are kinda working but should be removed and do try to use the id generated by db itself

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,13 @@
+import mongoose from "mongoose";
+import app from "./app.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-import express from "express";
-import cors from "cors";
-import connectDB from "./config/db.js";
-import jobRoutes from "./routes/job.routes.js";
-
-console.log("SERVER FILE URI =", process.env.MONGO_URI); // debug
-
-const app = express();
-
-// Connect DB **AFTER dotenv.config**
-connectDB();
-
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use("/api/jobs", jobRoutes);
-
-app.post("/api/jobs", (req, res) => {
-  console.log("BODY:", req.body); // DEBUG
-  res.send("received");
-});
-
-
-app.get("/", (req, res) => {
-  res.send({ message: "MSL's Backend server is running 🚀" });
-});
-
-const PORT = process.env.PORT || 4080;
-app.listen(PORT, () =>
-  console.log(`Server running at http://localhost:${PORT}`)
-);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(4080, () => console.log("Server running on 4080"));
+  })
+  .catch((err) => console.log(err));
