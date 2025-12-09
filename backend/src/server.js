02f5fec1ff7543cd109a5157972362ca5262372f
Anand <anand@git.system>
2025-12-09 17:29:16 +0530

    Added CRUD API for jobs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,35 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import jobRoutes from "./routes/job.routes.js";
+
+console.log("SERVER FILE URI =", process.env.MONGO_URI); // debug
 
 const app = express();
 
-// Connect Database
-const connectDB = require("./config/db");
+// Connect DB **AFTER dotenv.config**
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-// Test route
+// Routes
+app.use("/api/jobs", jobRoutes);
+
+app.post("/api/jobs", (req, res) => {
+  console.log("BODY:", req.body); // DEBUG
+  res.send("received");
+});
+
+
 app.get("/", (req, res) => {
   res.send({ message: "MSL's Backend server is running 🚀" });
 });
 
 const PORT = process.env.PORT || 4080;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
